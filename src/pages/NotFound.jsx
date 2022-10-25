63cc17ade873e153d83720fb92930e5e0d474a8e
Nicole Barranca <nikkibarranca@gmail.com>
2022-10-24 20:42:09 -0400

    notfound page button back to home

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -5,7 +5,12 @@ function NotFound() {
     <div className="hero">
       <div className="text-cetner hero-content">
         <div className="max-w-lg">
-          <h1 className="text-8xl font-bold mb-8"> Oops</h1>
+          <h1 className="text-8xl font-bold mb-8"> Oops!</h1>
+          <p className="text-5xl mb-8">404 - Page not found!</p>
+          <Link to="/" className="btn btn-primary btn-lg">
+            <FaHome className="mr-2" />
+            Back To Home
+          </Link>
         </div>
       </div>
     </div>
