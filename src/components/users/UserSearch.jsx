87d1ff9c9a8ea54de52255e94f027d412acc3f90
Nicole Barranca <nikkibarranca@gmail.com>
2022-11-07 11:45:54 -0500

    form and form state

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { useState } from "react";
 
 function UserSearch() {
-  return <div>Search</div>;
-}
+  const [text, setText] = useState("");
 
+  const handleChange = (e) => setText(e.target.value);
+
+  return (
+    <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
+      <div>
+        <form>
+          <div className="form-control">
+            <div className="relative">
+              <input
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
+                type="text"
+                placeholder="Search"
+                value={text}
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                className="absolute top-0 ruight-0 rounded-l-none w-36 btn btn-lg"
+              >
+                Go
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+      <div>
+        {/* button is not visable currently */}
+        <button className="btn btn-ghost btn-lg">Clear</button>
+      </div>
+    </div>
+  );
+}
 export default UserSearch;
