aad5c38b470ca69bcd6761a2f57d7492a9f626e5
Nicole Barranca <nikkibarranca@gmail.com>
2022-11-07 10:35:35 -0500

    coverted to global state using context

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -2,17 +2,17 @@ import { createContext, useState } from "react";
 
 const GithubContext = createContext();
 
-const GITHUB_URL = process.env.REACT_APP - GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP - GITHUB_TOKEN;
+const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
+    const response = await fetch(`${GITHUB_URL}/users`, {
       headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
@@ -21,4 +21,12 @@ export const GithubProvider = ({ children }) => {
     setUsers(data);
     setLoading(false);
   };
+
+  return (
+    <GithubContext.Provider value={{ users, loading, fetchUsers }}>
+      {children}
+    </GithubContext.Provider>
+  );
 };
+
+export default GithubContext;
