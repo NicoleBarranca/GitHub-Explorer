4e836442e90e0efb9f2d59a111e172f5562c026d
Nicole Barranca <nikkibarranca@gmail.com>
2022-11-07 11:56:52 -0500

    clear button shows when there are users in state

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,26 +1,41 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import GithubContext from "../../context/github/GithubContext";
 
 function UserSearch() {
   const [text, setText] = useState("");
 
+  const { users } = useContext(GithubContext);
+
   const handleChange = (e) => setText(e.target.value);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (text === "") {
+      alert("Please enter something");
+    } else {
+      // @todo
+
+      setText("");
+    }
+  };
 
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8">
       <div>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="form-control">
             <div className="relative">
               <input
-                className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 type="text"
+                className="w-full pr-40 bg-gray-200 input input-lg text-black"
                 placeholder="Search"
                 value={text}
                 onChange={handleChange}
               />
               <button
                 type="submit"
-                className="absolute top-0 ruight-0 rounded-l-none w-36 btn btn-lg"
+                className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg"
               >
                 Go
               </button>
@@ -28,10 +43,11 @@ function UserSearch() {
           </div>
         </form>
       </div>
-      <div>
-        {/* button is not visable currently */}
-        <button className="btn btn-ghost btn-lg">Clear</button>
-      </div>
+      {users.length > 0 && (
+        <div>
+          <button className="btn btn-ghost btn-lg">Clear</button>
+        </div>
+      )}
     </div>
   );
 }
