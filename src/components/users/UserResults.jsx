86c81072dba20c1b995f429d258362b32538247e
Nicole Barranca <nikkibarranca@gmail.com>
2022-11-06 20:54:40 -0500

    conditional for loading user account names

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -21,13 +21,18 @@ function UserResults() {
     setUsers(data);
     setLoading(false);
   };
-  return (
-    <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
-      {users.map((user) => (
-        <h3>{user.login}</h3>
-      ))}
-    </div>
-  );
+
+  if (!loading) {
+    return (
+      <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
+        {users.map((user) => (
+          <h3>{user.login}</h3>
+        ))}
+      </div>
+    );
+  } else {
+    <h3>Loading... Spin Spin Spin</h3>;
+  }
 }
 
 export default UserResults;
